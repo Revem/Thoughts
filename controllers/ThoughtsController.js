c2580505f92b5c80cc98e6b6eabe1c5792bfa716
= <=>
2023-09-11 22:32:34 -0300

    Lastest update

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -3,7 +3,9 @@ const User = require('../models/User')
 
 module.exports = class ThoughtController {
   static async showThoughts(req, res) {
-    res.render('thoughts/home')
+    const thoughtsData = await Thought.findAll({
+      include: User,
+    })
   }
 
   static async dashboard(req, res) {
@@ -21,8 +23,13 @@ module.exports = class ThoughtController {
       res.redirect('/login')
     }
 
-    const thoughts = user.Thoughts.map((result) => result.dataValues)
-    console.log(thoughts)
+    const thoughts = user.Thoughts.map((result) => result.get({ plain: true }))
+
+    let emptyThoughts = false
+
+    if (thoughts.length === 0) {
+      emptyThoughts = true
+    }
 
     res.render('thoughts/dashboard', { thoughts })
   }
@@ -30,6 +37,8 @@ module.exports = class ThoughtController {
   static createThought(req, res) {
     res.render('thoughts/create')
   }
+
+
 
   static async createThoughtSave(req, res) {
     const thought = {
@@ -52,4 +61,43 @@ module.exports = class ThoughtController {
 
   }
 
+  static async removeThought(req, res) {
+    const id = req.body.id
+    const UserId = req.session.userid
+
+    try {
+      await Thought.destroy({ where: { id: id, UserId: UserId } })
+      req.flash('message', 'Pensamento removido com sucesso!')
+
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  static async updateThought(req, res) {
+    const id = req.params.id
+    const thought = Thought.findOne({ where: { id: id }, raw: true })
+
+    res.render('thoughts/edit', { thought })
+  }
+
+  static async updateThoughtSave(req, res) {
+    const id = req.body.id
+    const thought = {
+      title: req.body.title
+    }
+
+    try {
+      await Thought.update(thought, { where: { id: id } })
+      req.flash('message', 'Pensamento atualizadodo com sucesso!')
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 }

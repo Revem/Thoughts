e3c36838b8b7ab16b4d7d857f852e61a91cde166
Victor Picon <revemvi@hotmail.com>
2023-09-12 18:26:01 -0300

    feat: Project Done

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -1,13 +1,9 @@
 const Thought = require('../models/Thought')
 const User = require('../models/User')
 
-module.exports = class ThoughtController {
-  static async showThoughts(req, res) {
-    const thoughtsData = await Thought.findAll({
-      include: User,
-    })
-  }
+const { Op } = require('sequelize')
 
+module.exports = class ThoughtsController {
   static async dashboard(req, res) {
     const userId = req.session.userid
 
@@ -19,85 +15,118 @@ module.exports = class ThoughtController {
       plain: true,
     })
 
-    if (!user) {
-      res.redirect('/login')
+    const thoughts = user.Thoughts.map((result) => result.dataValues)
+
+    let emptyThoughts = true
+
+    if (thoughts.length > 0) {
+      emptyThoughts = false
     }
 
-    const thoughts = user.Thoughts.map((result) => result.get({ plain: true }))
+    console.log(thoughts)
+    console.log(emptyThoughts)
 
-    let emptyThoughts = false
-
-    if (thoughts.length === 0) {
-      emptyThoughts = true
-    }
-
-    res.render('thoughts/dashboard', { thoughts })
+    res.render('thoughts/dashboard', { thoughts, emptyThoughts })
   }
 
   static createThought(req, res) {
     res.render('thoughts/create')
   }
 
-
-
-  static async createThoughtSave(req, res) {
+  static createThoughtSave(req, res) {
     const thought = {
       title: req.body.title,
-      UserId: req.session.userid
+      UserId: req.session.userid,
     }
 
-
-    try {
-      await Thought.create(thought)
-
-      req.flash('message', 'Pensamento criado com sucesso!')
-
-      req.session.save(() => {
-        res.redirect('/thoughts/dashboard')
+    Thought.create(thought)
+      .then(() => {
+        req.flash('message', 'Pensamento criado com sucesso!')
+        req.session.save(() => {
+          res.redirect('/thoughts/dashboard')
+        })
       })
-    } catch (err) {
-      console.log(err)
-    }
-
+      .catch((err) => console.log())
   }
 
-  static async removeThought(req, res) {
+  static showThoughts(req, res) {
+    console.log(req.query)
+
+    // check if user is searching
+    let search = ''
+
+    if (req.query.search) {
+      search = req.query.search
+    }
+
+    // order results, newest first
+    let order = 'DESC'
+
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    } else {
+      order = 'DESC'
+    }
+
+    Thought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
+    })
+      .then((data) => {
+        let thoughtsQty = data.length
+
+        if (thoughtsQty === 0) {
+          thoughtsQty = false
+        }
+
+        const thoughts = data.map((result) => result.get({ plain: true }))
+
+        res.render('thoughts/home', { thoughts, thoughtsQty, search })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  static removeThought(req, res) {
     const id = req.body.id
-    const UserId = req.session.userid
 
-    try {
-      await Thought.destroy({ where: { id: id, UserId: UserId } })
-      req.flash('message', 'Pensamento removido com sucesso!')
-
-      req.session.save(() => {
-        res.redirect('/thoughts/dashboard')
+    Thought.destroy({ where: { id: id } })
+      .then(() => {
+        req.flash('message', 'Pensamento removido com sucesso!')
+        req.session.save(() => {
+          res.redirect('/thoughts/dashboard')
+        })
       })
-    } catch (err) {
-      console.log(err)
-    }
+      .catch((err) => console.log())
   }
 
-  static async updateThought(req, res) {
+  static updateThought(req, res) {
     const id = req.params.id
-    const thought = Thought.findOne({ where: { id: id }, raw: true })
 
-    res.render('thoughts/edit', { thought })
+    Thought.findOne({ where: { id: id }, raw: true })
+      .then((thought) => {
+        res.render('thoughts/edit', { thought })
+      })
+      .catch((err) => console.log())
   }
 
-  static async updateThoughtSave(req, res) {
+  static updateThoughtSave(req, res) {
     const id = req.body.id
+
     const thought = {
-      title: req.body.title
+      title: req.body.title,
+      description: req.body.description,
     }
 
-    try {
-      await Thought.update(thought, { where: { id: id } })
-      req.flash('message', 'Pensamento atualizadodo com sucesso!')
-      req.session.save(() => {
-        res.redirect('/thoughts/dashboard')
+    Thought.update(thought, { where: { id: id } })
+      .then(() => {
+        req.flash('message', 'Pensamento atualizado com sucesso!')
+        req.session.save(() => {
+          res.redirect('/thoughts/dashboard')
+        })
       })
-    } catch (err) {
-      console.log(err)
-    }
+      .catch((err) => console.log())
   }
 }
--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
-const flash = require('express-flash')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
-const app = express()
+const app = express();
 
-const conn = require('./db/conn')
+const conn = require("./db/conn");
 
-const Thought = require('./models/Thought')
-const User = require('./models/User')
+const Thought = require("./models/Thought");
 
-const thoughtsRoutes = require('./routes/thoughtsRoutes')
-const authRoutes = require('./routes/authRoutes')
+const thoughtsRoutes = require("./routes/thoughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
+const ThoughtsController = require("./controllers/ThoughtsController");
 
-const ThoughtsController = require('./controllers/ThoughtsController')
-
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(
   session({
-    name: "session",
-    secret: "nosso_secret",
+    name: 'session',
+    secret: 'nosso_secret',
     resave: false,
     saveUninitialized: false,
     store: new FileStore({
@@ -39,33 +37,35 @@ app.use(
     }),
     cookie: {
       secure: false,
-      maxAge: 360000,
-      expires: new Date(Date.now() + 360000),
-      httpOnly: true
-    }
-  })
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
 )
 
-app.use(flash())
+app.use(flash());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
+  console.log(req.session.userid);
+
   if (req.session.userid) {
-    res.locals.session = req.session
+    res.locals.session = req.session;
   }
-  next()
-})
 
-app.use('/', authRoutes)
-app.use('/thoughts', thoughtsRoutes)
+  next();
+});
 
-app.get('/', ThoughtsController.showThoughts)
+app.use("/thoughts", thoughtsRoutes);
+app.use("/", authRoutes);
+
+app.get("/", ThoughtsController.showThoughts);
 
 conn
-  // .sync({ force: true })
   .sync()
   .then(() => {
-    app.listen(3000)
+    app.listen(3000);
   })
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
